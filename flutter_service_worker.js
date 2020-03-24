89d9b0a8b28f684429e2fd593277c9e2a9d1c6b1
Thorsten Lorenz <thlorenz@gmx.de>
2020-03-23 21:12:36 -0600

    player: gestures + collision

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -2,7 +2,7 @@
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "88bc388509fdbc7e8442fa297342aaae",
-"main.dart.js": "a6c9b0f6c578a6389536961c4beb84fd",
+"main.dart.js": "e1d03c08d7ac36df85aa24d5ff5f21e9",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
