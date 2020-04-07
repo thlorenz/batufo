0afbfaa48e4fa9f19a192b9bfdd27014f37ff3f2
Thorsten Lorenz <thlorenz@gmx.de>
2020-04-06 19:24:18 -0600

    model: temporarily removing stats from game model

--- lib/shared/models/create_model.dart
+++ lib/shared/models/create_model.dart
@@ -1,14 +1,10 @@
 import 'package:batufo/shared/arena/arena.dart';
-import 'package:batufo/shared/game_props.dart';
 import 'package:batufo/shared/models/bullet_model.dart';
 import 'package:batufo/shared/models/game_model.dart';
-import 'package:batufo/shared/models/stats_model.dart';
 
 GameModel createModel(Arena arena, int clientID) {
-  final statsModel = StatsModel(playerHealth: GameProps.playerTotalHealth);
   final bullets = <BulletModel>[];
   return GameModel(
-    stats: statsModel,
     bullets: bullets,
   );
 }
--- lib/shared/models/game_model.dart
+++ lib/shared/models/game_model.dart
@@ -1,14 +1,11 @@
 import 'package:batufo/shared/dart_types/dart_types.dart';
 import 'package:batufo/shared/models/bullet_model.dart';
 import 'package:batufo/shared/models/player_model.dart';
-import 'package:batufo/shared/models/stats_model.dart';
 
 class GameModel {
   Map<int, PlayerModel> players;
-  StatsModel stats;
   List<BulletModel> bullets;
   GameModel({
-    @required this.stats,
     @required this.bullets,
   });
 
@@ -20,7 +17,6 @@ class GameModel {
     return '''GameModel {
     player: $players
     bullets: $bullets
-    stats: $stats
     }''';
   }
 }
