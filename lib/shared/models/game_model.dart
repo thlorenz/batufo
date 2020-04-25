bbe7e99a410fe636825410618834ceb1ad8e1119
Thorsten Lorenz <thlorenz@gmx.de>
2020-04-24 19:31:53 -0600

    stats: preparing player stats including observable

--- lib/shared/models/game_model.dart
+++ lib/shared/models/game_model.dart
@@ -1,18 +1,54 @@
+import 'dart:async';
+
+import 'package:batufo/shared/dart_types/dart_types.dart';
 import 'package:batufo/shared/diagnostics/logger.dart';
 import 'package:batufo/shared/models/game_state.dart';
+import 'package:batufo/shared/models/player_model.dart';
 
 final _log = Log<ClientGameState>();
 
 const SYNC_ONLY_ONCE = false;
 
+class Stats {
+  final double health;
+
+  Stats({@required this.health});
+
+  String toString() {
+    return 'Stats [ $health ]';
+  }
+
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Stats &&
+          runtimeType == other.runtimeType &&
+          health == other.health;
+
+  int get hashCode => health.hashCode;
+}
+
 class ClientGameState extends GameState {
+  final int clientID;
+
   bool synced = false;
+  Stats _currentStats;
+  final StreamController<Stats> _stats$;
+
+  ClientGameState({@required this.clientID})
+      : _stats$ = StreamController<Stats>();
+
+  Stream<Stats> get stats$ => _stats$.stream.asBroadcastStream();
+
   void sync(GameState serverState) {
     for (final entry in serverState.players.entries) {
       final player = entry.value;
+      final id = entry.key;
       if (!synced || !SYNC_ONLY_ONCE) {
-        players[entry.key] = player;
+        players[id] = player;
         synced = true;
+      }
+      if (id == clientID) {
+        _addStats(player);
       }
     }
 
@@ -20,6 +56,13 @@ class ClientGameState extends GameState {
     for (final bullet in serverState.bullets) {
       addBullet(bullet);
     }
+  }
+
+  void _addStats(PlayerModel player) {
+    final stats = Stats(health: player.health);
+    if (stats == _currentStats) return;
+    _currentStats = stats;
+    _stats$.add(stats);
   }
 
   String toString() {
