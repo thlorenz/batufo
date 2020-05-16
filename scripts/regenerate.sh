7a983bfbf4878ace74d6dbdb77f8c1e79cb6602f
Thorsten Lorenz <thlorenz@gmx.de>
2020-05-15 19:26:17 -0600

    chore: updating proto generator to include ts and js

--- scripts/regenerate.sh
+++ scripts/regenerate.sh
@@ -5,9 +5,24 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Instructions from: https://grpc.io/docs/quickstart/dart/
 # Install protoc: https://github.com/google/protobuf/releases
 # Install protoc plugin for dart: pub global activate protoc_plugin
+# Install protoc plugin for typescript: https://github.com/improbable-eng/ts-protoc-gen
+# Install google-protobuf https://www.npmjs.com/package/google-protobuf
 
 echo "Make sure to run this from the root of the project"
 
+# typescript
+protoc_gen_ts_path="$DIR/../server/node_modules/.bin/protoc-gen-ts"
+
+ts_generated_dir=$DIR/../server/src/generated
+rm -f "${ts_generated_dir}/*"
+
+# dart
 dart_generated_dir=$DIR/../client/lib/rpc/generated
-rm -rf "${generated_dir}/*"
-protoc --dart_out=grpc:"${dart_generated_dir}" -Iprotos protos/message_bus.proto
+rm -f "${dart_generated_dir}/*"
+
+protoc \
+    --plugin="protoc-gen-ts=${protoc_gen_ts_path}" \
+    --js_out="import_style=commonjs,binary:${ts_generated_dir}" \
+    --ts_out="${ts_generated_dir}" \
+    --dart_out="${dart_generated_dir}" \
+    -Iprotos protos/message_bus.proto
