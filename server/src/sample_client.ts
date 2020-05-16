e02368c61f869fe14e491d79e8a907189633d546
Thorsten Lorenz <thlorenz@gmx.de>
2020-05-15 20:17:46 -0600

    todo: updating

--- server/src/sample_client.ts
+++
@@ -1,7 +0,0 @@
-import io from 'socket.io-client'
-const socket = io.connect('http://localhost:2222')
-
-socket.on('game:started', (data: any) => {
-  console.log(data)
-  socket.emit('game:request', { my: 'data' })
-})
