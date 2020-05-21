48b31ad4d2c320bb4a4c6de3dafefaa7e9250c94
Thorsten Lorenz <thlorenz@gmx.de>
2020-05-20 23:30:49 -0600

    socket.io: adapting protoc buffer serialization
    - this works around the fact that the dart socket.io library treats
      every message as JSON
    - we basically send the data as a string instead of UIntArray from the
      server which prevents dart to split it into a map
    - on the dart side we pull the list back out of that string via a
      split/parse int operation
    
    Server Side (`toString()` is the adaptation):
    
    ```ts
    playingClient.serializeBinary().toString()
    ```
    
    Client Side:
    
    ```dart
    List<int> listFromData(dynamic data) {
      return (data as String).split(',').map(int.parse).toList();
    }
    final list = listFromData(data);
    final client = PlayingClient.fromBuffer(list);
    ```
    
    Fortunately this is only necessary for messages coming from the server
    as those are the ones converted by the dart lib. When sending messages
    to the server it's not necessary.

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,7 @@
 import debug from 'debug'
 import http from 'http'
 import socketio from 'socket.io'
-import { PlayingClient } from './generated/message_bus_pb'
+import { PlayingClient, PlayRequest } from './generated/message_bus_pb'
 
 const PORT = process.env.PORT || 2222
 const logInfo = debug('app:info')
@@ -23,11 +23,17 @@ io.on('connection', (socket: socketio.Socket) => {
   const playingClient = new PlayingClient()
   playingClient.setGameid(1111)
   playingClient.setClientid(2222)
-  socket.emit('game:started', playingClient.serializeBinary())
-  socket.on('play:request', (_req) => {
-    // logInfo('got play request')
-    // logInfo('got play request for level %s', req.getLevelname())
+  socket.on('play:request', (data) => {
+    const req = PlayRequest.deserializeBinary(data)
+    logInfo('got play request for level [%s]', req.getLevelname())
   })
+
+  // Need to send this as string to be able to deserialize on the dart side
+  // without it splitting things into a JSON map.
+  // This is all probably more expensive than it needed to be if the dart socket.io
+  // wouldn't try to treat everything as JSON, but the siz of data sent is
+  // roughly the same.
+  socket.emit('game:started', playingClient.serializeBinary().toString())
 })
 
 app.listen(PORT)
