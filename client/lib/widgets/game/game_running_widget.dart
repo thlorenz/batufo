0c3e5e1a9cbe3ca7a6a8778cad379e6428c3ad7d
Thorsten Lorenz <thlorenz@gmx.de>
2020-05-29 22:42:07 -0600

    client: fixing game gestures
    - wrapping with gesture detector to make that work

--- client/lib/widgets/game/game_running_widget.dart
+++ client/lib/widgets/game/game_running_widget.dart
@@ -1,5 +1,6 @@
 import 'package:batufo/engine/game_widget.dart';
 import 'package:batufo/game/client_game.dart';
+import 'package:batufo/game/inputs/gestures.dart';
 import 'package:flutter/material.dart';
 
 class GameRunningWidget extends StatelessWidget {
@@ -9,7 +10,10 @@ class GameRunningWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Stack(children: [
-      GameWidget(game),
+      GestureDetector(
+          onPanUpdate: GameGestures.instance.onPanUpdate,
+          onTap: GameGestures.instance.onTap,
+          child: GameWidget(game)),
     ]);
   }
 }
