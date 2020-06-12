7eacc8d24e89fb4bfac4ff9626cbe8fa4fa5c215
Thorsten Lorenz <thlorenz@gmx.de>
2020-06-11 20:29:44 -0600

    editor: rendering canvas grid

---
+++ level-editor/src/grid.ts
@@ -0,0 +1,41 @@
+type Canvas = CanvasRenderingContext2D
+
+export class Grid {
+  constructor(
+    readonly _tileSize: number,
+    readonly marginCols: number = 5.0,
+    readonly marginRows: number = 5.0,
+  ) {}
+
+  _preparePaint(canvas: Canvas) {
+    canvas.strokeStyle = 'black'
+    canvas.lineWidth = 1
+  }
+
+  render(canvas: Canvas, nrows: number, ncols: number) {
+    const delta = this._tileSize
+    const gameWidth = ncols * delta
+    const gameHeight = nrows * delta
+    const marginX = this.marginCols * delta
+    const marginY = this.marginRows * delta
+    const startX = -marginX
+    const endX = gameWidth + marginX
+    const startY = -marginY
+    const endY = gameHeight + marginY
+
+    this._preparePaint(canvas)
+    for (let col = startX; col <= endX; col += delta) {
+      this._drawLine(canvas, col, startY, col, endY)
+    }
+    for (let row = startY; row <= endY; row += delta) {
+      this._drawLine(canvas, startX, row, endX, row)
+    }
+  }
+
+  _drawLine(canvas: Canvas, x1: number, y1: number, x2: number, y2: number) {
+    canvas.beginPath()
+    canvas.moveTo(x1, y1)
+    canvas.lineTo(x2, y2)
+    canvas.stroke()
+  }
+}
--- level-editor/src/main.ts
+++ level-editor/src/main.ts
@@ -1,2 +1,60 @@
-const canvas = document.getElementById('canvas')
-console.log(canvas)
+import { Grid } from './grid'
+
+class Level {
+  readonly _grid: Grid
+  _nrows: number
+  _ncols: number
+
+  constructor(
+    readonly canvasElement: HTMLCanvasElement,
+    readonly canvas: CanvasRenderingContext2D,
+    readonly _tileSize: number
+  ) {
+    this._grid = new Grid(tileSize)
+    this._nrows = 0
+    this._ncols = 0
+  }
+
+  update(nrows: number, ncols: number) {
+    this._nrows = nrows
+    this._ncols = ncols
+    this.canvasElement.width = this._ncols * this._tileSize
+    this.canvasElement.height = this._nrows * this._tileSize
+  }
+
+  render() {
+    this._renderBackground()
+    this._grid.render(this.canvas, this._nrows, this._ncols)
+  }
+
+  _renderBackground() {
+    this.canvas.fillStyle = 'white'
+    this.canvas.fillRect(0, 0, this.canvasElement.width,
+      this.canvasElement.height)
+  }
+}
+
+class Editor {
+  constructor(
+    readonly level: Level,
+  ) {}
+
+  update(nrows: number, ncols: number) {
+    this.level.update(nrows, ncols)
+    this.level.render()
+  }
+}
+
+const tileSize = 40
+
+const canvasElement: HTMLCanvasElement = document.getElementById(
+  'canvas',
+) as HTMLCanvasElement
+const canvas = canvasElement.getContext('2d')!
+const level = new Level(canvasElement, canvas, tileSize)
+const editor = new Editor(level)
+const nrows = 20
+const ncols = 10
+editor.update(nrows, ncols)
+
+console.log('running')
