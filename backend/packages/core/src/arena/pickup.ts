36bb9c19167601b26553e1c0e1482271dc42ecc0
Thorsten Lorenz <thlorenz@gmx.de>
2020-06-18 23:13:14 -0600

    messages: added pickup type with pack/unpack methods

---
+++ backend/packages/core/src/arena/pickup.ts
@@ -0,0 +1,48 @@
+import { strict as assert } from 'assert'
+import { TilePosition } from './tile-position'
+import {
+  PackedPickup,
+  PackedPickupType,
+  PackedPickupTypeMap,
+} from '../generated/message_bus_pb'
+
+export enum PickupType {
+  Medkit,
+  Shield,
+}
+
+function unpackPickupType(
+  data: typeof PackedPickupType[keyof PackedPickupTypeMap]
+): PickupType {
+  return data
+}
+
+function packPickupType(
+  type: PickupType
+): typeof PackedPickupType[keyof PackedPickupTypeMap] {
+  return type
+}
+
+export class Pickup {
+  constructor(readonly type: PickupType, readonly tilePosition: TilePosition) {}
+
+  pack(): PackedPickup {
+    const packedType = packPickupType(this.type)
+    const packedTilePosition = this.tilePosition.pack()
+
+    const packedPickup = new PackedPickup()
+    packedPickup.setType(packedType)
+    packedPickup.setTileposition(packedTilePosition)
+    return packedPickup
+  }
+
+  static unpack(data: PackedPickup): Pickup {
+    const type = unpackPickupType(data.getType())
+
+    const packedTilePosition = data.getTileposition()
+    assert(packedTilePosition != null, 'pickup cannot be missing tilePosition')
+    const tilePosition = TilePosition.unpack(packedTilePosition)
+
+    return new Pickup(type, tilePosition)
+  }
+}
