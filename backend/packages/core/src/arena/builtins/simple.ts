5623808918eb1a15b3aa6a4708124536dbbdcda8
Thorsten Lorenz <thlorenz@gmx.de>
2020-06-18 23:22:04 -0600

    server: adapting simple level to use shield pickup instead of diamond

--- backend/packages/core/src/arena/builtins/simple.ts
+++ backend/packages/core/src/arena/builtins/simple.ts
@@ -8,7 +8,7 @@ export default Level.fromLevelString(
 =                     =
 =====             =====
 =     ====        =
-=   d =  =        =
+=   s =  =        =
 =     ====        =
 =====             ========
 =   +                    =
