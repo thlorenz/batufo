072f8affc629e39d38c1e0e55d82b62dbeeb0566
Thorsten Lorenz <thlorenz@gmx.de>
2020-06-18 23:14:59 -0600

    client: adding pickup with pack/unpack methods

---
+++ client/lib/arena/pickup.dart
@@ -0,0 +1,39 @@
+import 'package:batufo/engine/tile_position.dart';
+import 'package:batufo/rpc/generated/message_bus.pb.dart'
+    show PackedPickup, PackedPickupType;
+
+enum PickupType {
+  Medkit,
+  Shield,
+}
+
+PickupType unpackPickupType(PackedPickupType data) {
+  return PickupType.values[data.value];
+}
+
+PackedPickupType packPickupType(PickupType type) {
+  return PackedPickupType.values[type.index];
+}
+
+class Pickup {
+  final PickupType type;
+  final TilePosition tilePosition;
+
+  Pickup(this.type, this.tilePosition);
+
+  PackedPickup pack() {
+    final packedType = packPickupType(type);
+    final packedTilePosition = tilePosition.pack();
+
+    return PackedPickup()
+      ..type = packedType
+      ..tilePosition = packedTilePosition;
+  }
+
+  static Pickup unpack(PackedPickup data) {
+    final type = unpackPickupType(data.type);
+    final tilePosition = TilePosition.unpack(data.tilePosition);
+
+    return Pickup(type, tilePosition);
+  }
+}
