5fb2f5195df8cf66dd73232e84a135ac8f89e052
Thorsten Lorenz <thlorenz@gmx.de>
2020-06-21 18:53:50 -0600

    client: enforcing 640x640 min size on desktop

--- client/lib/main/desktop.dart
+++ client/lib/main/desktop.dart
@@ -3,13 +3,18 @@ import 'package:batufo/game/sound/enabled_sound.dart';
 import 'package:batufo/main/main.dart';
 import 'package:batufo/setup/config.dart';
 import 'package:flutter/material.dart';
+import 'package:window_size/window_size.dart' show setWindowMinSize;
 
 Future<void> destopMainEntry(String serverIP) async {
   WidgetsFlutterBinding.ensureInitialized();
   final config = getConfig();
+  final minSize = Size(640, 640);
+  try {
+    setWindowMinSize(minSize);
+    // ignore: avoid_catches_without_on_clauses, empty_catches
+  } catch (err) {}
 
   final sound = await EnabledSound.create(audioAssets);
-
   return mainEntry(
     serverIP: serverIP,
     config: config,
