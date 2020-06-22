187e54a6989dcf37e0374a7854aef02daaff1b65
Thorsten Lorenz <thlorenz@gmx.de>
2020-06-21 18:53:30 -0600

    client: installed window_size plugin

--- client/macos/Flutter/GeneratedPluginRegistrant.swift
+++ client/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import window_size
 import audioplayers
 import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
   AudioplayersPlugin.register(with: registry.registrar(forPlugin: "AudioplayersPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
