cbf11072794ad16369b82882f06c544b6f104278
Thorsten Lorenz <thlorenz@gmx.de>
2020-06-24 17:08:11 -0600

    chore: adding web+sound entry point

---
+++ client/lib/main/web.debug+sound.dart
@@ -0,0 +1,26 @@
+import 'package:batufo/game/assets/assets.dart';
+import 'package:batufo/game/sound/enabled_sound.dart';
+import 'package:batufo/game_props.dart';
+import 'package:batufo/main/main.dart';
+import 'package:batufo/setup/config.dart';
+import 'package:flutter/material.dart';
+
+// ATM results in the below, keeping this entry point to
+// test regularly if things get fixed.
+
+// Overflow on channel: plugins.flutter.io/path_provider.
+// Messages on this channel are being discarded in FIFO fashion.
+// The engine may not be running or you need to adjust
+// the buffer size if of the channel.
+Future<void> main() async {
+  final serverIP = GameProps.localhost;
+  WidgetsFlutterBinding.ensureInitialized();
+  final config = getConfig(platform: PlatformType.Web);
+
+  final sound = await EnabledSound.create(audioAssets);
+  return mainEntry(
+    serverIP: serverIP,
+    config: config,
+    sound: sound,
+  );
+}
