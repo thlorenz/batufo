f2ae5f436559a42c772a5367fc3b52ab1a21c854
Thorsten Lorenz <thlorenz@gmx.de>
2020-06-24 17:27:26 -0600

    chore: update version script

---
+++ scripts/update-version.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+CLIENT=$DIR/../client
+
+VERSION=`grep -e 'version:' $CLIENT/pubspec.yaml | cut -c10-`
+
+echo "const BATUFO_VERSION = '$VERSION';" > $CLIENT/lib/generated_batufo_version.dart
+
+git add "$CLIENT/pubspec.yaml"
+git add "$CLIENT/lib/generated_batufo_version.dart"
+git commit -m "chore: update version v$VERSION"
+git tag "v$VERSION"
