6ac0baa3fe83f978a39e86f0aa00cce194b20ff7
Thorsten Lorenz <thlorenz@gmx.de>
2020-07-05 16:35:21 -0600

    client: adding url_launcher dep

--- client/macos/Flutter/GeneratedPluginRegistrant.swift
+++ client/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,11 @@ import Foundation
 import window_size
 import audioplayers
 import path_provider_macos
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
   AudioplayersPlugin.register(with: registry.registrar(forPlugin: "AudioplayersPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
